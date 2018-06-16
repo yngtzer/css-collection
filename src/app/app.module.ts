fed299efc5e8a7d7096ff1cbc3d779fdc8523f14
yngtzer <ta0003er@e.ntu.edu.sg>
2018-06-16 10:51:52 +0800

    resolve conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,21 +12,14 @@ import { MaterialUIModule } from './material-ui/material-ui.module';
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { SpinnerComponent } from './spinner/spinner.component';
-<<<<<<< HEAD
 import { SquareLoadingComponent } from './square-loading/square-loading.component';
-=======
->>>>>>> c4b781cef7d037ddca1869d0550b8ecac3b5e91a
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
-<<<<<<< HEAD
     SpinnerComponent,
     SquareLoadingComponent
-=======
-    SpinnerComponent
->>>>>>> c4b781cef7d037ddca1869d0550b8ecac3b5e91a
   ],
   imports: [
     BrowserModule,
